0e3f33d7d6fb5e836a580548e88ecd6f00957763
胡俊杰 <hujunjie02@bilibili.com>
2022-09-13 02:01:35 +0800

    修改插入探针逻辑

--- plugin/src/main/groovy/com/ttp/and_jacoco/JacocoTransform.groovy
+++ plugin/src/main/groovy/com/ttp/and_jacoco/JacocoTransform.groovy
@@ -68,7 +68,7 @@ class JacocoTransform extends Transform {
               //  gitPush(jacocoExtension.gitPushShell, "jacoco auto commit")
                 //获取差异方法集
                BranchDiffTask branchDiffTask = project.tasks.findByName('generateReport')
-             branchDiffTask.pullDiffClasses()
+          //   branchDiffTask.pullDiffClasses()
                 branchDiffTask.pullDiffadmin()
                 println('send http to diff-admin and get difffile')
             }
